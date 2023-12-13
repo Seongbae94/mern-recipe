9f56d6d885716bf8785fc3856c5262fff3fb9819
Seongbae94 <seongbaelee944@gmail.com>
2023-12-13 15:13:09 -0500

    install dotenv
    install dotenv to communicate with .env file

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,14 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 
 app.use(express.json());
 app.use(cors());
+
+mongoose.connect(process.env.MONGODB_URL);
 
 app.listen(3001, () => console.log("SERVER STARTED!"));
