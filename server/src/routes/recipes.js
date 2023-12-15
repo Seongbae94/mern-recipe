54931187e95f9b1c799ff9500ba8b2cba4af6715
Seongbae94 <seongbaelee944@gmail.com>
2023-12-15 18:18:36 -0500

    work on recipe route
    1. able to save a recipe
    2. able to get recipes

--- server/src/routes/recipes.js
+++ server/src/routes/recipes.js
@@ -1,9 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
 import { RecipeModel } from "../models/Recipes.js";
+import { UserModel } from "../models/Users.js";
 
 const router = express.Router();
 
+//get all recipes
 router.get("/", async (req, res) => {
   try {
     const allRecipes = await RecipeModel.find({});
@@ -13,12 +15,51 @@ router.get("/", async (req, res) => {
   }
 });
 
+//create a recipe
 router.post("/", async (req, res) => {
   const recipe = new RecipeModel(req.body);
 
   try {
     const response = await recipe.save();
     res.json(response);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+//save a recipe
+router.put("/", async (req, res) => {
+  try {
+    const recipe = await RecipeModel.findById(req.body.recipeID);
+    const user = await UserModel.findById(req.body.userID);
+
+    user.savedRecipes.push(recipe);
+    await user.save();
+    res.json({ savedRecipes: user.savedRecipes });
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+//get saved recipes Id
+router.get("/savedRecipes/ids", async (req, res) => {
+  try {
+    const user = await UserModel.findById(req.body.userID);
+    res.json({ savedRecipes: user?.savedRecipes });
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+//get whole saved recipes info
+router.get("/savedRecipes", async (req, res) => {
+  try {
+    const user = await UserModel.findById(req.body.userID);
+    const savedRecipes = await RecipeModel.find({
+      _id: { $in: user.savedRecipes },
+    });
+
+    res.json({ savedRecipes });
   } catch (error) {
     res.json(error);
   }
