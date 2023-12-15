302aa47644d0ca351656eba45fedf55e7ca14ee2
Seongbae94 <seongbaelee944@gmail.com>
2023-12-15 16:09:12 -0500

    work on apis
    1. able to see whole recipes (get)
    2. able to add a recipe (post)

--- server/src/index.js
+++ server/src/index.js
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 import { userRouter } from "./routes/users.js";
+import { recipesRouter } from "./routes/recipes.js";
 
 const app = express();
 dotenv.config();
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/auth", userRouter);
+app.use("/recipes", recipesRouter);
 
 mongoose.connect(process.env.MONGODB_URL);
 
---
+++ server/src/routes/recipes.js
@@ -0,0 +1,27 @@
+import express from "express";
+import mongoose from "mongoose";
+import { RecipeModel } from "../models/Recipes.js";
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  try {
+    const allRecipes = await RecipeModel.find({});
+    res.json(allRecipes);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+router.post("/", async (req, res) => {
+  const recipe = new RecipeModel(req.body);
+
+  try {
+    const response = await recipe.save();
+    res.json(response);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+export { router as recipesRouter };
